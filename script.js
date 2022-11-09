44682343efd9060ed60504c8726fd1a1c83458de
gazrobbo <39488148+gazrobbo@users.noreply.github.com>
2022-11-09 16:38:20 +0000

    yesno working

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 const calculateButton = document.querySelector('#calculate-button');
 let answer = document.getElementById('answer');
+const rearWall = document.getElementById("rear-wall-clad");
 
 calculateButton.addEventListener('click', () => {
   let roomHeight = Number(document.getElementById('room-height').value);
   let roomWidth = Number(document.getElementById('room-width').value);
   let roomDepth = Number(document.getElementById('room-depth').value);
-  // let rearWallClad = document.getElementById('rear-wall-clad');
-
+  let rearWallClad = rearWall.value;
+  console.log(rearWallClad);
 
   let result = (roomHeight * roomWidth * roomDepth);
   answer.innerHTML = (result);
