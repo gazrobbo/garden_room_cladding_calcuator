10375cf9c888c77017632d33e7e36649c766df59
gazrobbo <39488148+gazrobbo@users.noreply.github.com>
2022-11-10 14:29:15 +0000

    before removing all input clears

--- script.js
+++ script.js
@@ -1,43 +1,110 @@
 const calculateButton = document.querySelector('#calculate-button');
-let answer = document.getElementById('answer');
+const resetButton = document.querySelector('#reset-button');
+let claddingAreaResult = document.getElementById('cladding-area-result');
+let claddingLinearMetreResult = document.getElementById('cladding-linear-metre-result');
+let claddingBoardWidth = 0;
+let claddingBoardWidthMetre = 0
 
+let roomHeight = 0
+let roomWidth = 0
+let roomDepth = 0
+let claddingArea = 0
 
-calculateButton.addEventListener('click', () => {
+let doorHeight = 0
+let doorWidth = 0
+
+let window1Height = 0
+let window1Width = 0
+
+let window2Height = 0
+let window2Width = 0
+
+let window3Height = 0
+let window3Width = 0
+1
+function calculateArea(height, width) {
+  let area = height * width;
+  return area
+};
+
+function calculateCladdingArea() {
   // pulls in  room size
-  let roomHeight = Number(document.getElementById('room-height').value / 1000);
-  let roomWidth = Number(document.getElementById('room-width').value / 1000);
-  let roomDepth = Number(document.getElementById('room-depth').value / 1000);
+  roomHeight = Number(document.getElementById('room-height').value / 1000);
+  roomWidth = Number(document.getElementById('room-width').value / 1000);
+  roomDepth = Number(document.getElementById('room-depth').value / 1000);
   let rearWallClad = (document.getElementById("rear-wall-clad")).value;
   // find if rear wall needs to be calculated
-  let cladWalls = 5;
-  if (rearWallClad == "Yes") {
-    cladWalls = 2
-  } else {
+  let cladWalls = 2;
+  if (rearWallClad == "No") {
     cladWalls = 1
+  } else {
+    cladWalls = 2
   };
   // calculates room size
   let buildingSize = (roomHeight * roomWidth * 2) + (roomHeight * roomDepth * cladWalls);
 
   // pulls in  door size
-  let doorHeight = Number(document.getElementById('door-height').value / 1000);
-  let doorWidth = Number(document.getElementById('door-width').value / 1000);
-  let doorSize = doorHeight * doorWidth;
+  doorHeight = Number(document.getElementById('door-height').value / 1000);
+  doorWidth = Number(document.getElementById('door-width').value / 1000);
+  let doorSize = calculateArea(doorHeight, doorWidth);
 
   // pulls in  window size
-  let window1Height = Number(document.getElementById('window1-height').value / 1000);
-  let window1Width = Number(document.getElementById('window1-width').value / 1000);
-  let window1Size = window1Height * window1Width;
+  window1Height = Number(document.getElementById('window1-height').value / 1000);
+  window1Width = Number(document.getElementById('window1-width').value / 1000);
+  let window1Size = calculateArea(window1Height, window1Width);
 
-  let window2Height = Number(document.getElementById('window2-height').value / 1000);
-  let window2Width = Number(document.getElementById('window2-width').value / 1000);
-  let window2Size = window2Height * window2Width;
+  window2Height = Number(document.getElementById('window2-height').value / 1000);
+  window2Width = Number(document.getElementById('window2-width').value / 1000);
+  let window2Size = calculateArea(window2Height, window2Width);
 
-  let window3Height = Number(document.getElementById('window3-height').value / 1000);
-  let window3Width = Number(document.getElementById('window3-width').value / 1000);
-  let window3Size = window3Height * window3Width;
+  window3Height = Number(document.getElementById('window3-height').value / 1000);
+  window3Width = Number(document.getElementById('window3-width').value / 1000);
+  let window3Size = calculateArea(window3Height, window3Width);
+// claculates cladding area
+  claddingArea = buildingSize - doorSize - window1Size - window2Size - window3Size;
+  claddingAreaResult.innerHTML = (claddingArea);
+// calculates linear metre 
+  claddingBoardWidthMetre = Number(document.getElementById("cladding-board-width").value / 1000);
+  claddingLinearMetreResult.innerHTML = (claddingArea / claddingBoardWidthMetre);
+  console.log(claddingAreaResult);
+  console.log(claddingBoardWidth);
+  console.log(claddingBoardWidthMetre);
+};
 
-  console.log(cladWalls);
+calculateButton.addEventListener('click', () => {
+  calculateCladdingArea()
+});
 
-  let result = buildingSize - doorSize - window1Size - window2Size - window3Size;
-  answer.innerHTML = (result);
+// Resets values
+function resetValues() {
+  // document.getElementById('room-height').value = "";
+  // document.getElementById('room-width').value = "";
+  // document.getElementById('room-depth').value = "";
+  // document.getElementById('door-height').value = "";
+  // document.getElementById('door-width').value = "";
+
+  // document.getElementById('window1-height').value = "";
+  // document.getElementById('window1-width').value = "";
+
+  // document.getElementById('window2-height').value = "";
+  // document.getElementById('window2-width').value = "";
+
+  // document.getElementById('window3-height').value = "";
+  // document.getElementById('window3-width').value = "";
+
+  // document.getElementById('cladding-board-width').value = "";
+var fields = document.getElementsByTagName('input'),
+  length = fields.length;
+while (length--) {
+  if (fields[length].type === 'text') {
+    fields[length].value = '';
+  }
+}
+  claddingBoardWidth = 0;
+  claddingBoardWidthMetre = 0
+};
+
+resetButton.addEventListener('click', () => {
+
+  resetValues();
 });
